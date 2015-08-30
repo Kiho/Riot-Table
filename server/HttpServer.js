9cccffa9921afd3537028467e4a616eab4ff78b9
Kiho <kihoc22@gmail.com>
2015-08-30 13:53:32 -0700

    Update project reference.

--- server/HttpServer.js
+++ server/HttpServer.js
@@ -2,6 +2,7 @@
 /// <reference path="../typings/underscore/underscore.d.ts" />
 var http = require("http");
 var _ = require("underscore");
+
 function getPaginatedItems(items, p, s) {
     var page = p || 1, perPage = s || 10, offset = (page - 1) * perPage, paginatedItems = _.rest(items, offset).slice(0, perPage);
     return {
@@ -12,12 +13,14 @@ function getPaginatedItems(items, p, s) {
         data: paginatedItems
     };
 }
+
 var crossDomainHeaders = {
     'Content-Type': 'text/plain',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
     'Access-Control-Allow-Headers': 'Content-Type'
 };
+
 http.createServer(function (req, res) {
     var query = require('url').parse(req.url, true).query;
     var fs = require('fs');
@@ -25,15 +28,15 @@ http.createServer(function (req, res) {
     if (req.url === '/favicon.ico') {
         // console.log('Favicon was requested');
         res.end("");
-    }
-    else {
+    } else {
         fs.readFile('../data/liste1.json', 'utf8', function (err, data) {
             if (err)
                 throw err;
             items = JSON.parse(data);
             console.log("p : " + query.p);
             console.log("s : " + query.s);
-            //console.log('items length: ' + items.length);        
+
+            //console.log('items length: ' + items.length);
             var r = getPaginatedItems(items, parseInt(query.p), parseInt(query.s));
             var json = JSON.stringify(r);
             res.writeHead(200, crossDomainHeaders);
@@ -41,5 +44,6 @@ http.createServer(function (req, res) {
         });
     }
 }).listen(1337, '127.0.0.1');
+
 console.log('Server running at http://127.0.0.1:1337/');
 //# sourceMappingURL=HttpServer.js.map
